46f3e01d267a2763b4e927947d5f2d2760f5e38e
KajetanJanusz <134972496+KajetanJanusz@users.noreply.github.com>
2026-03-21 17:29:16 +0100

    Add models mixins (#7)
    * add base and outbox mixins with initial models
    
    * ruff
    
    * ruff

--- ecom_shop_shared_lib/models/outbox_mixin.py
+++ ecom_shop_shared_lib/models/outbox_mixin.py
@@ -1,6 +1,7 @@
 from enum import StrEnum
+from typing import Any
 
-from sqlalchemy import Enum, Integer, String
+from sqlalchemy import Enum, Integer, String, JSON
 from sqlalchemy.orm import Mapped, mapped_column
 
 from ecom_shop_shared_lib.models.base_mixin import BaseDbModelMixin
@@ -16,6 +17,7 @@ class EventStatus(StrEnum):
 
 class OutboxMixin(BaseDbModelMixin):
     event_type: Mapped[str] = mapped_column(String, nullable=False)
+    payload: Mapped[dict[str, Any]] = mapped_column(JSON, nullable=False)
     status: Mapped[str] = mapped_column(Enum(EventStatus), nullable=False)
     attempts: Mapped[int] = mapped_column(Integer, default=1)
     last_error: Mapped[str | None] = mapped_column(String, nullable=True, default=None)
